3fa24bc34c66040238ddcbe6db08a8523ed86062
triviere19 <triviere19@comcast.net>
2025-09-04 23:31:12 -0400

    Add optional corner numbers to cells

--- src/components/Crossword/Cell/Cell.tsx
+++ src/components/Crossword/Cell/Cell.tsx
@@ -1,19 +1,22 @@
 import { CrosswordCellState } from "@/models/Crossword";
 import styles from "./Cell.module.css";
-import { KeyboardEvent } from "react";
+import { KeyboardEvent, MouseEvent, Ref } from "react";
 
 
 export interface CellProps {
+    number?: number | undefined,
     value: string,
     state: CrosswordCellState,
     size: number,
     answer: string,
     onChange?: (set: string) => void,
     onFocus?: () => void,
-    onClick?: () => void,
+    onClick?: (event: MouseEvent) => void,
     onBlur?: () => void,
     onKeyDown?: (event: KeyboardEvent) => void,
     highlighted?: boolean,
+    ref?: Ref<HTMLInputElement>,
+    id?: string,
 }
 
 
@@ -28,21 +31,31 @@ export default function Cell(props: CellProps){
     }
 
     return (
-        <input 
-            className={`${styles.cell} ${stateClassName} ${props.highlighted ? styles.highlighted : ""}`}
-            style={{
-                width: `${props.size}px`,
-                height: `${props.size}px`,
-                fontSize: `${props.size * 0.75}px`, /** chat says most font' letters fit in 70-80% of a pixel */
-            }}
-            // defaultValue={props.answer == "-" ? "" : props.answer.toUpperCase()}
-            value={props.value}
-            disabled={props.answer == "-"}
-            onChange={(e) => props.onChange?.(e.target.value.charAt(e.target.value.length-1).toUpperCase())}
-            onClick={props.onClick}
-            onFocus={props.onFocus}
-            onBlur={props.onBlur}
-            onKeyDown={props.onKeyDown}
-        />
+        <div className={styles.container}>
+            <input 
+                className={`${styles.cell} ${stateClassName} ${props.highlighted ? styles.highlighted : ""}`}
+                style={{
+                    width: `${props.size}px`,
+                    height: `${props.size}px`,
+                    fontSize: `${props.size * 0.65}px`, /** chat says most font' letters fit in 70-80% of a pixel */
+                }}
+                // defaultValue={props.answer == "-" ? "" : props.answer.toUpperCase()}
+                value={props.value}
+                disabled={props.answer == "-"}
+                onChange={(e) => props.onChange?.(e.target.value.charAt(e.target.value.length-1).toUpperCase())}
+                onClick={props.onClick}
+                onFocus={props.onFocus}
+                onBlur={props.onBlur}
+                onKeyDown={props.onKeyDown}
+                ref={props.ref}
+                id={props.id}
+            />
+            <div 
+                className={styles.number}
+                style={{
+                    fontSize:`${props.size*0.25}px`,
+                }}
+            >{props.number}</div>
+        </div>
     );
 }
